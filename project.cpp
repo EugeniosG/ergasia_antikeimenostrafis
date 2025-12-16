165f53fe362e5bd7cb8db045c5bff9537a9313fa
Manos120610 <manoszervas01@gmail.com>
2025-12-16 22:02:49 +0200

    Classes

--- project.cpp
+++ project.cpp
@@ -1,14 +1,55 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 
 using namespace std;
-
-
 
 void print_help() {
 
 }
+//Κλάση για στατικά αντικείμενα
+class StaticObject {
+private:
+    string id;
+    char glyph;
+public:
+    StaticObject() {
+        cout << "StaticObject initialized" << endl;
+    }
+    ~StaticObject() {
+        cout << "StaticObject destroyed" << endl;
+    }
+}
 
+//Κλάση για κινητά αντικείμενα
+class MovingObject { 
+private:
+    int speed;
+    int direction;
+public:
+    MovingObject() {
+        cout << "MovingObject initialized" << endl;
+    }
+    ~MovingObject() {
+        cout << "MovingObject destroyed" << endl;
+    }
+}
+
+//Κλάση για αυτόνομο αυτοκίνητο
+class Car : public MovingObject {
+    private: 
+        int speed;
+        int direction;
+    public:
+        Car() {
+            cout << "Car initialized" << endl;
+        }
+        ~Car() {
+            cout << "Car destroyed" << endl;
+        }
+}
+
+
+//Αρχή της main 
 int main(int argc, char* argv[]){
     
     if (argv[1] == "--help") {
