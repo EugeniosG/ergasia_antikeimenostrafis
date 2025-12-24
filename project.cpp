9bcd8de28faae6afd5ff37a7cf8a1f9197c75300
EugeniosG <eugeniosg6@yahoo.com>
2025-12-24 15:31:25 +0200

    update

--- project.cpp
+++ project.cpp
@@ -4,21 +4,22 @@
 using namespace std;
 
 void print_help() {
-    cout << "Usage:" << endl;
     cout << "--seed <n>                     Randome seed (default current time)" << endl;
     cout << "--dimX <n>                     World width (default 40)" << endl;
     cout << "--dimY <n>                     World height (default 40)" << endl;
     cout << "--numMovingCars <n>            Number of moving cars (default 3)" << endl;
     cout << "--numMovingBikes <n>           Number of moving bikes (default 4)" << endl;
-    cout << "--numParkedCars <n>            Number of moving bikes (default 7)" << endl;
+    cout << "--numParkedCars <n>            Number of parked cars (default 7)" << endl;
     cout << "--numStopSigns <n>             Number of signs STOP (deafault 1)" << endl;
     cout << "--numTraficLights <n>          Number of trafic light (default 2)" <<  endl;
     cout << "--simulatorticks <n>           Maximun simulator ticks (default 100)" << endl;
     cout << "--gps <x1> <y1> [x2 y2  ...]   Gps target coordinates (required)" << endl;
     cout << "--help                         Showing this message" << endl;
-    cout << "example Usage:" << endl;
-    cout << "./project --seed 10 --dimX 40 --simulatorticks 200" << endl;
+    cout << "Usage:" << endl;
+    cout << "./project --seed <n> --dimX <n> --dimY <n> --numMovingCars <n> --numMovingBikes <n> --numParkedCars <n> --numStopSigns <n> --numTraficLights <n> --simulatorticks <n> --gps <x1> <y1> [x2 y2  ...] " << endl;
 }
+
+
 
 //Κλάση για στατικά αντικείμενα
 class StaticObject {
@@ -53,16 +54,16 @@ public:
 };
 
 //Κλάση για αυτόνομο αυτοκίνητο
-class Car : public MovingObject {
+class SelfDrivingCar : public MovingObject {
     private: 
 
         int speed;
         int direction;
     public:
-        Car() {
+        SelfDrivingCar() {
             cout << "Car initialized" << endl;
         }
-        ~Car() {
+        ~SelfDrivingCar() {
             cout << "Car destroyed" << endl;
         }
 };
@@ -70,19 +71,62 @@ class Car : public MovingObject {
 
 //Αρχή της main 
 int main(int argc, char* argv[]){
+    //βαζω τις default τιμες στις μεταβλητες και επιτα ελεγχω αν  μου εχει δωσει κατι διαφορετικο ο χρηστης στην γραμμη εντολων
+    //κατα την κληση του προγραμματος, και ενημερωνω την αντιστοιχη μεταβλητη
+    int dimX = 40;
+    int dimY = 40;
+    int MovingCars = 3;
+    int ParkedCars = 7;
+    int MovingBikes = 4;
+    int STOP = 1;
+    int TraficLights = 2;
+    int ticks = 100;
     //εκτυπωνω το βοηθητικο προς τον χρηστη αν το ζητησει
     if (strcmp(argv[1], "--help") == 0) {
         print_help();
     }
 
-    //παιρνω τις συντεταγμενες του αυτοκινουμενου οχημετος που μου εχει δωσει ο χρηστης 
+    //παιρνω τις παραμετρους απο την γραμμη εντολων που μου εχει δωσει ο χρηστης 
     int i = 1; 
     while(1){
+        if (strcmp(argv[i], "--dimX")){
+            dimX = stoi(argv[i+1]);
+        }
+
+        if (strcmp(argv[i], "--dimY")){
+            dimY = stoi(argv[i+1]);
+        }
+
+        if (strcmp(argv[i], "--numMovingCars") == 0){
+            MovingCars = stoi(argv[i+1]);
+        }
+
+        if (strcmp(argv[i], "--numMovingBikes") == 0){
+            MovingBikes = stoi(argv[i+1]);
+        }
+
+        if (strcmp(argv[i], "--numParkedCars") == 0){
+            ParkedCars = stoi(argv[i+1]);
+        }
+
+        if(strcmp(argv[i], "--numStopSigns") == 0){
+            STOP = stoi(argv[i+1]);
+        }
+
+        if(strcmp(argv[i], "--numTraficLights") == 0){
+            TraficLights = stoi(argv[i+1]);
+        }
+
+        if(strcmp(argv[i], "--simulatorticks") == 0){
+            ticks = stoi(argv[i+1]);
+        }
+
         if (strcmp(argv[i], "--gps") == 0){
             int x = stoi(argv[i+1]);
             int y = stoi(argv[i+2]);
-            continue;
+            break;;
         }
+
         i++;
     }
 
